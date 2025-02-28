362215253de9774afbe7f6c7971e08a982b655ac
Alberto <acortazar@centrosanluis.com>
2025-02-28 09:23:14 +0100

    Add files via upload

--- primeraWeb/src/main/java/com/centrosanluis/controller/ListadoController.java
+++ primeraWeb/src/main/java/com/centrosanluis/controller/ListadoController.java
@@ -13,29 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/listadoUsuarios")
 public class ListadoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ListadoController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//llamo al service para obtener todos los usuarios
+		
+		//añado en la request el atributo (no sesion) listado usuarios
+		
+		//encadeno la peticion y la envio a listado.jsp
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
